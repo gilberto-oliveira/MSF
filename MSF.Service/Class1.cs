3448e0c2a9de8c15d1c2fe76a43ad3b53c8cead9
gilberto-oliveira <gpoproducoes@gmail.com>
2020-06-28 19:27:40 -0300

    Add project files.

---
+++ MSF.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MSF.Service
+{
+    public class Class1
+    {
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Projeto para Monitoramento de Equipamentos
+
+- ASP.NET Core 2.2
+- Angular 7.2
